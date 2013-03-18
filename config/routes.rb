28f8cd54b66683590a6bae5a572460e4bb5caf86
samo <samo@samo-TravelMate-5320.(none)>
2013-03-18 08:25:00 +0100

    Files on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Sharebox::Application.routes.draw do
 
+  resources :assets
+
   devise_for :users  do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  #this route is for file downloads
+  match "assets/get/:id" => "assets#get", :as => "download"
 
   root :to => "home#index"
 
